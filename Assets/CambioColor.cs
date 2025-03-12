9b7d7260e1e5d20f053006be15cf1d846f55de3d
IUrielCH <iucoria1607@gmail.com>
2025-03-11 18:19:33 -0600

    V3
    Cambio del Image Target
    Modificación del tamaño de la araña para mejor visibilidad en la nueva imagen

--- Assets/CambioColor.cs
+++ Assets/CambioColor.cs
@@ -7,6 +7,8 @@ public class CambioColor : MonoBehaviour
     public GameObject Spider;
     public Color color;
     public Material colorMaterial;
+    public int textRand;
+    public Texture[] texturas;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,9 @@ public class CambioColor : MonoBehaviour
 
     public void CambioColor_SPD()
     {
-        Spider.GetComponentInChildren<Renderer>().material.color = color;
+        textRand = Random.Range(0, texturas.Length);
+        Spider.GetComponentInChildren<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        Spider.GetComponentInChildren<Renderer>().material.mainTexture = texturas[textRand];
         //colorMaterial.color = color; Para modificar múltiples elementos a la vez
     }
 }
---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVg5NEFCWC8vLy8vQUFBQm1YeDFMUndweWtoMnJUWDZYRUJXbCtKWTNuVVd5MG1PMlZkOFhzZUhRSnV1THRnWGhkY2Y1c1dkcGEyMWRCTHhjSzNJZTV4V2xlVFlmS1hsS2FsMUdlalF6RmFYdVRsMHkwblpWMDhuaFhubURtV0ZwL09UWWxMMzhvVHpnWkQ2TE9mYkl3aFB0WkhuZTAxd2FBQkZKK1JNdGhUWFVXVzBaL1M2TTRLMURUaVV1L1hzL2xJUitzU3puR3VtQnJjQTJrZGF1b3VzbzVRaHJsNnBDcWNCV3dJNzgyNGVqUlJyaTRabHFscHN6eW83RXBWa0ltdHpkblM3UDZMTDFUNFZMS0dZdXJkT3hNNUpFWFFQYlRNaEVDRFE3UE8yMzE5NEllNDlxZG81cEdjR1g5UlA3cE5EV3FjNDcySkdaQUV0OHVTY3c1R0RiMzVlT0VMQlN1T1dBYU5OcTBMdEVERE54WTJsZXNlRFNtdGo=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
